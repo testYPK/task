d5475ace17ded8900d6c72adc5b78f0701952f41
Yuri Katovich <yuroff19@gmail.com>
2024-01-25 07:26:49 +0200

    Update SettingsTableSeeder.php

--- database/seeders/SettingsTableSeeder.php
+++ database/seeders/SettingsTableSeeder.php
@@ -7,43 +7,38 @@ use Illuminate\Database\Seeder;
 
 class SettingsTableSeeder extends Seeder
 {
+    private array $settings = [
+        [
+            'key' => 'path',
+            'label' => 'File location',
+            'value' => 'csvFiles',
+        ],
+        [
+            'key' => 'file_name_pattern',
+            'label' => 'File name pattern',
+            'value' => 'csv_file',
+        ],
+        [
+            'key' => 'load_enabled',
+            'label' => 'Load enabled',
+            'type' => 'checkbox',
+            'value' => true,
+        ],
+        [
+            'key' => 'load_schedule',
+            'label' => 'Load schedule time',
+            'type' => 'time',
+            'value' => '14:00',
+        ],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        Settings::create(
-                [
-                    'key' => 'path',
-                    'label' => 'File location',
-                    'value' => 'csvFiles'
-                ]
-        );
-
-        Settings::create(
-            [
-                'key' => 'file_name_pattern',
-                'label' => 'File name pattern',
-                'value' => 'csv_file'
-            ]
-        );
-
-        Settings::create(
-            [
-                'key' => 'load_enabled',
-                'label' => 'Load enabled',
-                'type' => 'checkbox',
-                'value' => true,
-            ]
-        );
-
-        Settings::create(
-            [
-                'key' => 'load_schedule',
-                'label' => 'Load schedule time',
-                'type' => 'time',
-                'value' => '14:00',
-            ]
-        );
+        foreach ($this->settings as $setting) {
+            Settings::create($setting);
+        }
     }
 }
